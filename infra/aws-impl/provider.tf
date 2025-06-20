51051efded86f5b55baa0b6c30526b0dd909629c
Kyle Pettigrew <kylepettigrew@gmail.com>
2025-06-20 17:17:12 +1000

    add debug steps, lock provider versions

--- infra/aws-impl/provider.tf
+++ infra/aws-impl/provider.tf
@@ -20,7 +20,6 @@ provider "aws" {
 
 terraform {
   required_version = ">= 1.3"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -42,7 +41,7 @@ terraform {
   backend "s3" {
     bucket = "examplebucket-fhir-aws"
     region = "ap-southeast-2"
-    # key    = "infra/inferno/au-fhir-inferno-sparkey-prod.tfstate"
+    # key     = "infra/inferno/au-fhir-inferno-dev.tfstate"
     # profile = "FHIR-Admin"
   }
 }
